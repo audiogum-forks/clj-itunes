a3c0864e575cabb3a7a074cca721cef9b2ff6f42
Andrew Jones <github@a4j.xyz>
2018-10-16 11:24:09 +0100

    Version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject audiogum/clj-itunes "0.1.1"
+(defproject audiogum/clj-itunes "0.1.2-SNAPSHOT"
   :description "A Clojure client for the iTunes search API"
   :url "http://github.com/cosmin/clj-itunes"
   :license {:name "Eclipse Public License"
